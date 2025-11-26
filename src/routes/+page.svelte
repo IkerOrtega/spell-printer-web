86af3853b0b14afcce4aabca433e6d302f2b4bfb
bgd <bberghie@gmail.com>
2025-11-26 22:03:11 +0100

    poc GET

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,35 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+	//import type { Spell } from './api/spells/Spell';
+
+	const options = {
+		method: 'GET'
+	};
+
+	const spells = fetch('http://localhost:8080/api/v1/spell', options)
+		.then((response) => response.json())
+		.then((response) => console.log(response))
+		.catch((err) => console.error(err));
+</script>
+
+{#await spells then}
+	{console.log(spells)}
+{/await}
+
+<h1>Hola mundo</h1>
+
+<style>
+	h1 {
+		font-family:
+			system-ui,
+			-apple-system,
+			BlinkMacSystemFont,
+			'Segoe UI',
+			Roboto,
+			Oxygen,
+			Ubuntu,
+			Cantarell,
+			'Open Sans',
+			'Helvetica Neue',
+			sans-serif;
+	}
+</style>
---
+++ src/routes/api/spells/Spell.ts
@@ -0,0 +1,19 @@
+export interface Spell {
+	englishTitle: string;
+	title: string;
+	actionType: string;
+	type: string;
+	level: string;
+	traits: string[];
+	traditions: string[];
+	castTime: string;
+	muse: string;
+	trigger: string;
+	range: string;
+	objectives: string;
+	area: string;
+	defense: string;
+	duration: string;
+	description: string;
+	heightenings: string;
+}
