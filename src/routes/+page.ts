62160c2386fbbe89bb4b435ec1653a6e4ebcaedf
bgd <bberghie@gmail.com>
2025-12-28 15:29:55 +0100

    ?

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -3,7 +3,7 @@ import type { PageLoad } from './$types';
 import type { SpellResponse } from './api/spells/SpellResponse';
 
 export const load: PageLoad = async () => {
-	const res = await fetch('');// TODO Use .env
+	const res = await fetch('???');// TODO Use .env
 	const data: SpellResponse = await res.json();
 	return {
 		spells: data.spellList
